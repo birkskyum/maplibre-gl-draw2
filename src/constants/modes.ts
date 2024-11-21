6c07a511d55d90df54d3c6cf6af314a1842c83e3
Birk Skyum <birk.skyum@pm.me>
2024-11-21 03:41:54 +0100

    modes

--- src/constants/modes.ts
+++ src/constants/modes.ts
@@ -1,9 +1,52 @@
 
+
+
 export const ModeStrings = {
-  DRAW_LINE_STRING: "draw_line_string",
-  DRAW_POLYGON: "draw_polygon",
-  DRAW_POINT: "draw_point",
+
+  /**
+   * Lets you select, delete, and drag features.
+   *
+   * In this mode, features can have their selected state changed by the user.
+   *
+   * Draw is in `simple_select` mode by default, and will automatically transition
+   * into `simple_select` mode again every time the user finishes drawing a feature
+   * or exits `direct_select` mode.
+   */
   SIMPLE_SELECT: "simple_select",
+  /**
+   * By default MapLibreDraw ships with a few modes. These modes aim to cover the
+   * basic needed functionally for MapLibreDraw to create the core GeoJSON feature
+   * types. Along with these, MapLibreDraw also supports
+   * [custom modes. Click here for more details](https://github.com/mapbox/maplibre-gl-draw/blob/main/docs/MODES.md).
+   */
+  /**
+   * Lets you draw a LineString feature.
+   */
+  DRAW_LINE_STRING: "draw_line_string",
+
+  /**
+   * Lets you draw a Polygon feature.
+   */
+  DRAW_POLYGON: "draw_polygon",
+
+  /**
+   * Lets you draw a Point feature.
+   */
+  DRAW_POINT: "draw_point",
+
+  /**
+   * Lets you select, delete, and drag vertices; and drag features.
+   *
+   * `direct_select` mode does not apply to point features, because they have no
+   * vertices.
+   *
+   * Draw enters `direct_select` mode when the user clicks a vertex of a selected
+   * line or polygon. So `direct_select` mode typically follows `simple_select` mode.
+   */
   DIRECT_SELECT: "direct_select",
+
+  /**
+   * A static mode where no interactions are allowed.
+   */
   STATIC: "static",
 };
