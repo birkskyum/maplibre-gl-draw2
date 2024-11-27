4b401908520a3ed59a077dd5aa58eb73810fce11
Birk Skyum <birk.skyum@pm.me>
2024-11-27 14:43:12 +0100

    use defineconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-export default {
+import {defineConfig} from "vite";
+
+export default defineConfig({
   root: ".",
   base: ".",
   server: {
@@ -13,4 +15,4 @@ export default {
       },
     },
   },
-};
+});
