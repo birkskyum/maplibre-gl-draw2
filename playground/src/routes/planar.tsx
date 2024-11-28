8b910c087e49947d48ed5494ad2873ebcdd9a636
Birk Skyum <birk.skyum@pm.me>
2024-11-28 17:31:18 +0100

    move click handlers to buttons

--- playground/src/routes/planar.tsx
+++ playground/src/routes/planar.tsx
@@ -48,160 +48,94 @@ export default function ReinitControl() {
     });
     map.addControl(Draw as unknown as IControl, "top-right");
 
-    map.on("load", () => {
-      const startPoint = document.getElementById("start-point");
-      startPoint!.onclick = function () {
-        Draw.changeMode("draw_point");
-      };
-
-      const startLine = document.getElementById("start-line");
-      startLine!.onclick = function () {
-        Draw.changeMode("draw_line_string");
-      };
-
-      const startRectangle = document.getElementById("start-rectangle");
-      startRectangle!.onclick = function () {
-        Draw.changeMode("draw_rectangle");
-      };
-
-      const startAssistedRectangle = document.getElementById(
-        "start-assisted-rectangle"
-      );
-      startAssistedRectangle!.onclick = function () {
-        Draw.changeMode("draw_assisted_rectangle");
-      };
-
-      const startPolygon = document.getElementById("start-polygon");
-      startPolygon!.onclick = function () {
-        Draw.changeMode("draw_polygon");
-      };
-
-      // const startCircle = document.getElementById("start-circle");
-      // startCircle!.onclick = function () {
-      //   Draw.changeMode("draw_circle");
-      // };
-
-      const startCircleRadius = document.getElementById(
-        "start-draw-circle-radius"
-      );
-      startCircleRadius!.onclick = function () {
-        Draw.changeMode("draw_circle_radius");
-      };
-
-      const startDragCircle = document.getElementById("start-drag-circle");
-      startDragCircle!.onclick = function () {
-        Draw.changeMode("drag_circle");
-      };
-
-      const startRSRectangle = document.getElementById("start-scale-rotate");
-      startRSRectangle!.onclick = function () {
-        Draw.changeMode("scale_rotate");
-      };
-
-      const staticMode = document.getElementById("static");
-      staticMode!.onclick = function () {
-        Draw.changeMode("static");
-      };
     });
-  });
-  return (
-    <>
-      <div class="flex h-screen w-full">
-        <div class="w-[250px] bg-gray-100 p-5">
-          {/* <h2>Pages</h2> */}
-          <nav>
-            <p class="text-xl">Installation</p>
+    return (
+      <>
+        <div class="flex h-screen w-full">
+          <div class="w-[250px] bg-gray-100 p-5">
+            <nav>
+              <p class="text-xl">Installation</p>
 
-            <ul class="list-disc p-0">
-              <li>
-                <Button
-                  onClick={() => {
-                    if (drawIsActive) return;
-                    drawIsActive = true;
-                    map.addControl(Draw as unknown as IControl, "top-right");
-                  }}
-                >
-                  Add draw
-                </Button>
-              </li>
-              <li>
-                <Button
-                  onClick={() => {
-                    if (!drawIsActive) return;
-                    drawIsActive = false;
-                    map.removeControl(Draw as unknown as IControl);
-                  }}
-                >
-                  Remove draw
-                </Button>
-              </li>
-            </ul>
+              <ul class="list-disc p-0">
+                <li>
+                  <Button
+                    onClick={() => {
+                      if (drawIsActive) return;
+                      drawIsActive = true;
+                      map.addControl(Draw as unknown as IControl, "top-right");
+                    }}
+                  >
+                    Add draw
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    onClick={() => {
+                      if (!drawIsActive) return;
+                      drawIsActive = false;
+                      map.removeControl(Draw as unknown as IControl);
+                    }}
+                  >
+                    Remove draw
+                  </Button>
+                </li>
+              </ul>
 
-            <p class="text-xl  mt-3">Draw</p>
-            <ul class="list-disc p-0">
-              <li>
-                <Button id="start-point">Point</Button>
-              </li>
-              <li>
-                <Button id="start-line">Line</Button>
-              </li>
-              <li>
-                <Button id="start-polygon">Polygon</Button>
-              </li>
+              <p class="text-xl  mt-3">Draw</p>
+              <ul class="list-disc p-0">
+                <li>
+                  <Button onClick={() => Draw.changeMode("draw_point")}>Point</Button>
+                </li>
+                <li>
+                  <Button onClick={() => Draw.changeMode("draw_line_string")}>Line</Button>
+                </li>
+                <li>
+                  <Button onClick={() => Draw.changeMode("draw_polygon")}>Polygon</Button>
+                </li>
+                <li>
+                  <Button onClick={() => Draw.changeMode("draw_rectangle")}>2-Click Rectangle</Button>
+                </li>
+                <li>
+                  <Button onClick={() => Draw.changeMode("draw_assisted_rectangle")}>
+                    3-Click "Assisted" Rectangle
+                  </Button>
+                </li>
+                <li>
+                  <Button onClick={() => Draw.changeMode("drag_circle")}>Drag Circle</Button>
+                </li>
+                <li>
+                  <Button onClick={() => Draw.changeMode("draw_circle_radius")}>
+                    Draw Circle Radius
+                  </Button>
+                </li>
+              </ul>
 
-              <li>
-                <Button id="start-rectangle"> 2-Click Rectangle</Button>
-              </li>
-              <li>
-                <Button id="start-assisted-rectangle">
-                  3-Click "Assisted" Rectangle
-                </Button>
-              </li>
-              {/* <li>
-              <Button id="start-circle">1-Click Circle</Button>
-            </li> */}
-              <li>
-                <Button id="start-drag-circle">Drag Circle</Button>
-              </li>
-              <li>
-                <Button id="start-draw-circle-radius">
-                  Draw Circle Radius
-                </Button>
-              </li>
-            </ul>
+              <p class="text-xl mt-3">Edit</p>
 
-            <p class="text-xl mt-3">Edit</p>
+              <ul class="list-disc p-0">
+                <li>
+                  <Button onClick={() => Draw.changeMode("scale_rotate")}>Scale and Rotate</Button>
+                </li>
+                <li>
+                  <A href="/planar/scale-rotate" class="no-underline text-gray-700">
+                    Combine
+                  </A>
+                </li>
+                <li>
+                  <A href="/planar/scale-rotate" class="no-underline text-gray-700">
+                    Uncombine
+                  </A>
+                </li>
+              </ul>
 
-            <ul class="list-disc p-0">
-              <li>
-                <Button id="start-scale-rotate">Scale and Rotate</Button>
-              </li>
-              <li>
-                <A
-                  href="/planar/scale-rotate"
-                  class="no-underline text-gray-700"
-                >
-                  Combine
-                </A>
-              </li>
-              <li>
-                <A
-                  href="/planar/scale-rotate"
-                  class="no-underline text-gray-700"
-                >
-                  Uncombine
-                </A>
-              </li>
-            </ul>
-
-            <p class="text-xl mt-3">Read-only</p>
-            <ul class="list-disc p-0">
-              <li>
-                <Button id="static">Static Mode</Button>
-              </li>
-            </ul>
-          </nav>
-        </div>
+              <p class="text-xl mt-3">Read-only</p>
+              <ul class="list-disc p-0">
+                <li>
+                  <Button onClick={() => Draw.changeMode("static")}>Static Mode</Button>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
         <div id="map" class="h-full w-full"></div>
       </div>
